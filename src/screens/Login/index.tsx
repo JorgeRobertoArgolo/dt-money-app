2fca6904109e52f0ca5dbc124d3e57c0b3272640
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-02-03 14:21:14 -0300

    config: Configuração inicial do projeto

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const Login = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>Tela de login</Text>
+        </View>
+    );
+}
