90fb46176df1374b81a82faa231ac985d476c6fc
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-02-09 10:26:41 -0300

    feat: Design da tela de Registrar-se

---
+++ src/screens/Register/RegisterForm/index.tsx
@@ -0,0 +1,79 @@
+import { useForm } from "react-hook-form";
+import { View, Text } from "react-native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+
+import { AuthHeader } from "@/components/AuthHeader"
+import { AppInput } from "@/components/AppInput";
+import { AppButton } from "@/components/AppButton";
+import { PublicStackParamsList } from "@/routes/PublicRoutes";
+
+interface FormRegisterParams {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export const RegisterForm = () => {
+
+    const {
+        control,
+        handleSubmit,
+        formState: { isSubmitting}
+    } = useForm<FormRegisterParams>()
+
+    const navigation = useNavigation<NavigationProp<PublicStackParamsList>>();
+
+    return (
+        <>
+            <AuthHeader />
+
+            <AppInput
+                control={control}
+                name="name"
+                label="NOME"
+                placeholder="Seu nome"
+                leftIconName="person-outline"
+            />
+
+            <AppInput
+                control={control}
+                name="email"
+                label="EMAIL"
+                placeholder="mail@example.com"
+                leftIconName="mail-outline"
+            />
+
+            <AppInput
+                control={control}
+                name="password"
+                label="SENHA"
+                placeholder="Sua senha"
+                leftIconName="lock-outline"
+                secureTextEntry
+            />
+
+            <AppInput
+                control={control}
+                name="confirmPassword"
+                label="CONFIRMAR SENHA"
+                placeholder="Confirme sua senha"
+                leftIconName="lock-outline"
+                secureTextEntry
+            />
+
+            <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+                <AppButton mode="fill" iconName="arrow-forward">
+                    Cadastrar
+                </AppButton>
+        
+                <View>
+                    <Text className="mb-6 text-gray-300 text-base">Já possui uma conta ?</Text>
+                    <AppButton mode="outline" iconName="arrow-forward" onPress={() => navigation.navigate("Login")}>
+                        Acessar
+                    </AppButton>
+                </View>
+            </View>
+        </>
+    )
+}
