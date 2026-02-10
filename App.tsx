958a61b6d801dfff6416edee208f9955e1c7a7b1
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-02-10 10:13:36 -0300

    config: criando e configurando contexto da snackbar

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import NavigationRoutes from "@/routes";
 import "./src/styles/global.css"
 import { Login } from '@/screens/Login';
 import { AuthContextProvider } from "@/context/auth.context";
+import { SnackbarContextProvider } from "@/context/snackbar.context";
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <NavigationRoutes />
-    </AuthContextProvider>
+    <SnackbarContextProvider>
+      <AuthContextProvider>
+        <NavigationRoutes />
+      </AuthContextProvider>
+    </SnackbarContextProvider>
   );
 }
