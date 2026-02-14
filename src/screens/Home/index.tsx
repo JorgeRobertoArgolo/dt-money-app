91a5749e122b5828c8dd9c708cff6b7383b07cce
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-02-14 08:57:36 -0300

    feat: Header das telas autenticadas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,19 @@
+import { AppHeader } from "@/components/AppHeader";
 import { useAuthContext } from "@/context/auth.context";
-import { Text, View, TouchableOpacity } from "react-native"
+import { Text, TouchableOpacity } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
 
     const { handleLogout } = useAuthContext();
 
     return (
-        <View>
+        <SafeAreaView className="flex-1 bg-background-primary">
+            <AppHeader />
             <Text>Home</Text>
             <TouchableOpacity onPress={() => handleLogout()}>
                 <Text>Logout</Text>
             </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     )
 }
