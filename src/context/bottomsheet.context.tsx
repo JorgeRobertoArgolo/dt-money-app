44ba9940d30aefb1709a2bad4461af7c44b8c5d5
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-02-14 09:36:38 -0300

    config: Configurando o bottom sheet context

---
+++ src/context/bottomsheet.context.tsx
@@ -0,0 +1,30 @@
+import { createContext, FC, PropsWithChildren, use, useCallback, useState } from "react";
+
+interface BottomSheetContextType {
+    openBottomSheet: (content: React.ReactNode, index: number) => void;
+    closeBottomSheet: () => void;
+}
+
+export const BottomSheetContext = createContext({} as BottomSheetContextType);
+
+export const BottomSheetProvider: FC<PropsWithChildren> = ({ children }) => {
+    
+    const [content, setContent] = useState<React.ReactNode | null>(null);
+
+    const openBottomSheet = useCallback((newContent: React.ReactNode, index: number) => {
+        setContent(newContent);
+    }, []);
+
+    const closeBottomSheet = useCallback(() => {setContent(null)}, []);
+
+    return (
+        <BottomSheetContext.Provider
+            value={{
+                openBottomSheet,
+                closeBottomSheet,
+            }}
+        >
+            {children}
+        </BottomSheetContext.Provider>
+    );
+}
